4fd324e4896d4a24b699b76864db197ceeb348fa
Keagon Brinkhuis <keaggzb2v@gmail.com>
2019-12-25 18:00:57 +0200

    added all files

---
+++ README.md
@@ -0,0 +1,19 @@
+# A fictional travel website
+
+## Built Using:
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Features:
+
+- Landing Page
+- Partner info section
+- Holiday ideas section
+
+## Author
+
+> Keagon Brinkhuis
+
+To view demo [Click Here](https://keagon98.github.io/dreamshare-fictional-website.io/).
---
+++ js/script.js
@@ -0,0 +1,51 @@
+/********************** 
+ On Scroll Animation
+**********************/
+
+window.sr = ScrollReveal();
+sr.reveal('.middle-text', {
+    duration: 2500,
+    origin: 'top',
+    distance: '50px'
+});
+sr.reveal('.partner-1', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px'
+});
+
+sr.reveal('.partner-2', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px'
+});
+
+sr.reveal('.partner-3', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px'
+});
+
+sr.reveal('.partner-4', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '100px'
+});
+
+sr.reveal('.container-1', {
+  duration: 2500,
+  viewFactor: 0.2
+});
+
+sr.reveal('.container-2', {
+  duration: 2500,
+  delay: 200,
+  viewFactor: 0.3
+});
+
+sr.reveal('.container-3', {
+  duration: 2500,
+  delay: 500,
+  viewFactor: 0.4
+});
+
